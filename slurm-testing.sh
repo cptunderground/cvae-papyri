78f1f16b04142dfcbaa8bbc5dd17cf590b118d87
Jannik-cluster <jannik.jaberg@unibas.ch>
2022-04-21 23:16:18 +0200

    cluster testing

--- slurm-testing.sh
+++ slurm-testing.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
-#SBATCH --job-name=myrun     #Name of your job
+#SBATCH --job-name=cvae-cluster-testing     #Name of your job
 #SBATCH --cpus-per-task=1    #Number of cores to reserve
-#SBATCH --mem-per-cpu=1G     #Amount of RAM/core to reserve
-#SBATCH --time=00:01:00      #Maximum allocated time
-#SBATCH --qos=30min         #Selected queue to allocate your job
+#SBATCH --mem-per-cpu=16G     #Amount of RAM/core to reserve
+#SBATCH --time=06:00:00      #Maximum allocated time
+#SBATCH --qos=6hours         #Selected queue to allocate your job
 #SBATCH --output=myrun.o   #Path and name to the file for the STDOUT
 #SBATCH --error=myrun.e    #Path and name to the file for the STDERR
 
 ml Miniconda2
-source activate cvae-cluster-3.8                 
-python main.py    
+source activate MA                 
+python main.py cluster --epochs=3 --generate
